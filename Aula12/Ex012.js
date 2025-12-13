ffbe8c19b09ee0aa58332cfca161f6acb225660f
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2025-12-13 09:41:51 -0300

    Praticando Node.js
    Praticando minha lógica no Node.js

--- Aula12/Ex012.js
+++ Aula12/Ex012.js
@@ -1,10 +1,10 @@
-var hora = 5
-if (hora > 12){
-    console.log('Boa Tarde')
-}  else if(hora < 12){
-    console.log('Bom Dia')
-}   else if (hora >= 18) {
-    console.log('Boa Noite')
-}   else if (hora > 0 && hora < 6){
-    console.log('Boa Madrugada')
+var hora = 0
+if (hora < 12 && hora >= 6){
+    console.log(`Bom dia, agora são ${hora} horas`)
+} else if (hora > 12 && hora < 18){
+    console.log(`Boa tarde, agora são ${hora} horas`)
+} else if (hora >= 18){
+    console.log(`Boa noite, agora são ${hora}`)
+} else if (hora >= 0 && hora < 6){
+    console.log(`Boa madrugrada, agora são ${hora} horas`)
 }
