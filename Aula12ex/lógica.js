0ab501a9208147b905bff27f56727353f5cc9e87
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2025-12-16 23:34:17 -0300

    deixando meu site bonito

--- Aula12ex/lógica.js
+++ Aula12ex/lógica.js
@@ -1,12 +1,19 @@
-
-var guardahoracliente = 5
+function carregar(){
+var guardahoracliente = 15
 var escrevehora = document.querySelector('div#escrevehora')
+var addimgs = document.querySelector('div#imgs')
 if (guardahoracliente < 12 && guardahoracliente >= 6){
     escrevehora.innerHTML = `Agora são ${guardahoracliente} horas da manhã`
-} else if(guardahoracliente == 12 && guardahoracliente < 18){
+    addimgs.src = `img/manha-redonda-250.png`
+} else if(guardahoracliente >= 12 && guardahoracliente < 18){
     escrevehora.innerHTML = `Boa Tarde! Agora São ${guardahoracliente} horas`
-} else if(guardahoracliente == 18 && guardahoracliente < 23){
+    addimgs.src = `img/tarde-redonda-250.png`
+} else if(guardahoracliente >= 18 && guardahoracliente < 23){
     escrevehora.innerHTML = `Noite Boa! Agora são ${guardahoracliente} horas da noite`
-} else if(guardahoracliente == 0 && guardahoracliente < 6){
+    addimgs.src = `img/noite-redonda-250.png`
+} else if(guardahoracliente >= 0 && guardahoracliente < 6){
     escrevehora.innerHTML = `Vamo dormir campeão já são ${guardahoracliente} da madrugada. Ela não vai te ligar`
+    addimgs.src = `img/madrugada-redonda-250.png`
 }
+
+}   
