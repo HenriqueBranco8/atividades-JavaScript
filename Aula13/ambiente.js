a6e51598ac7720e33f129a60b9091adf358bb037
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2025-12-21 17:43:33 -0300

    Aprendendo estruturas de repetições

---
+++ Aula13/ambiente.js
@@ -0,0 +1,21 @@
+/* var contador = 1
+while (contador < 999999){
+     console.log(`Passo ${contador}`)
+     contador++
+}
+*/
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador ++
+}  while (contador <= 10)
+
+
+/* Console.log('Tudo bem?')
+ Console.log('Tudo bem?')
+ Console.log('Tudo bem?')
+ Console.log('Tudo bem?')
+ Console.log('Tudo bem?')
+ Console.log('Tudo bem?')
+*/
