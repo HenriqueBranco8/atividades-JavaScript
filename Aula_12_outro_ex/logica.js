c6c192d05665fe2d77d02f5a7dcf93707789abd8
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2025-12-21 14:30:39 -0300

    Ex Finalizado

--- Aula_12_outro_ex/logica.js
+++ Aula_12_outro_ex/logica.js
@@ -21,9 +21,15 @@ function verificar(){
     //Vai escrever o genero da pessoa no HTML
     var genero = ''
     
-    //Cria uma imagem pelo JS (JavaScript)
+    //Esse bloco Cria uma imagem pelo JS (JavaScript) e configura o estilo dela
     var img = document.createElement('img')
     img.setAttribute('id', 'foto')
+    img.style.borderRadius = '500px'
+    img.style.display = 'block'
+    img.style.margin = '20px auto'
+    img.style.width = '330px'
+    img.style.height = '280px'
+    
 
     //Se a opção selecionada for masculina vai executar esse bloco
     if(fsex[0].checked){
@@ -33,11 +39,13 @@ function verificar(){
             img.setAttribute('src', 'imgs/homem-crianca.jpg')
         } else if (idade >= 12 && idade < 18){
              //adolecente
-            
+            img.setAttribute('src', 'imgs/itme.jpeg')
         }else if (idade >= 18 && idade < 64){
             //adulto
+            img.setAttribute('src', 'imgs/homen-adulto.jpeg')
         } else {
             //idoso
+            img.setAttribute('src', 'imgs/homen-idoso.jpg')
         }
 
     //Se a opção selecionada for feminina vai executar esse bloco
@@ -45,16 +53,21 @@ function verificar(){
         genero = 'Feminino'
         if(idade >=0 && idade < 12){
             //criança
+            img.setAttribute('src', 'imgs/criança-menina.jpg')
         } else if (idade >= 12 && idade < 18){
              //adolecente
+             img.setAttribute('src', 'imgs/adolecente-mulher.jpg')
         }else if (idade >= 18 && idade < 59){
             //adulto
+            img.setAttribute('src', 'imgs/mulher-adulta.jpeg')
         } else {
             //idoso
+            img.setAttribute('src', 'imgs/idosa.jpg')
         }
     }
 
     //Escreve o resultado de tudo no HTML
+    res.style.textAlign ='center'
     res.innerHTML = `Você tem ${idade} anos e é do gênero ${genero}`
     res.appendChild(img)
 }
