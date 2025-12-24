26ca2d18aadb2166294e5b545015d78fc5869a76
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2025-12-24 14:24:19 -0300

    Fazendo os exercicios do meu jeito

--- Copia14/ex1/logica.js
+++ Copia14/ex1/logica.js
@@ -6,23 +6,34 @@ function contar(){
     var inputpasso = document.getElementById('passo')
     var res = document.getElementById('res')
 
-
-    if(inputinicio.value.length == 0 || inputfim.value.length == 0 || inputpasso.value.length == 0){
+    //Esse bloco verifica se todos os campos foram preenchidos 
+    if(inputinicio.value.length = 0 || inputfim.value.length == 0 || inputpasso.value.length == 0){
         alert('[Erro] por favor, adicione número em todas as caixas')
-    } else if(inputinicio <= inputfim){
+    
+    //Se todos os campos foram preenchidos vai verificar todos os inputs e guardar os valores
+    } else {
         var inicio = Number(inputinicio.value)
         var fim = Number(inputfim.value)
         var passo = Number(inputpasso.value)
 
-
-        for(var contador = inicio; contador <= fim; contador += passo){
-            res.innerHTML += `${contador} - `
+    // Se o input do inicio for menor, vai começar uma contagem de progressão
+    } if (inicio < fim){
+        for(var contador = inicio; contador < fim; contador += passo){
+            res.innerHTML += `${contador} \u{1F449} `
+           
         }
 
-    } else if (inputinicio > inputfim){
-        
+    //Se não, se o inicio for menor, vai começar uma contagem regressiva
+    } else if (inicio > fim){
         for(contador = inicio; contador > fim; contador -= passo){
-            res.innerHTML += `${contador}`
+            res.innerHTML += `${contador} \u{1F449}`
+            
         }
+
+    //Se o inicio for igual ao fim, vai dar erro!
+    } else if(inicio == fim){
+        alert('Erro! Você já está localizado no Final da corrida.')
     }
+     res.innerHTML += `\u{1F3C1}`
+    
 }
