4ffe6cbd3cf137c2ba34b79ca73fbb2caa83cb82
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-01-14 19:00:24 -0300

    brincando com js
    tentando fazer um menu pelo terminal

---
+++ Brincando_com_java_script/financas.js
@@ -0,0 +1,13 @@
+const readline = require('readLine');
+const rl = readline.creatInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function menu(){
+    console.clear();
+
+    console.log('1 - Opção one')
+};
+
+menu();
