c0cb715084d923dda116a2ba12f5a2c3329de8e7
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2026-02-01 15:30:18 -0300

    Aprofundando meu conhecimento em condições

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade limite permitida MULTADO')
+}
+console.log(`Dirija sempre com cinto de segurança`)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Lituania'
+
+if (pais == 'Brasil'){
+    console.log(`Vivendo no ${pais}`)
+    console.log('Você é Brasileiro')
+} else {
+    console.log(`Vivendo no ${pais}`)
+    console.log('Você é estrangeiro')
+}
