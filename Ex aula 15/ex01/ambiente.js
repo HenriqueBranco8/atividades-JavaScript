011a3467433a161e2136086b6694ad19d4f089df
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2026-02-14 21:58:37 -0300

    Finalizando o primeiro desafio de vetores

--- Ex aula 15/ex01/ambiente.js
+++ Ex aula 15/ex01/ambiente.js
@@ -1,7 +1,23 @@
-var numbers = [7, 42, 89, 13, 56, 91, 24, 68, 35, 77]
+const numbers = [7, 42, 89, 13, 56, 91, 24, 68, 35, 77]
 var mostra = 0
+var soma = 0
+
+for(mostra in numbers){
+
+    //Mostra o array completo. Posições e elementos correspondente a elas
+    console.log(`posição ${mostra} tem o valor ${numbers[mostra]}`)
 
 
-for(var par = mostra % 2; mostra < numbers.length; par++){
-    console.log(`par ${numbers[mostra]}`)
+    //Vai mostrar todos os números pares
+    var par = numbers[mostra] % 2
+    if(par == 0){
+        console.log(`par: ${numbers[mostra]}`)
+    } 
 }
+
+//Soma todos os números dentro do vetor.
+for(var todos = 0; todos < numbers.length; todos++){
+        soma+= numbers[todos]
+        
+}
+console.log(`Soma de todos os números: ${soma}`)
