e7e91b437d051322f4670b03664812209cea9238
HenriqueBranco888 <henriquebrancodasilvadias@yahoo.com>
2026-02-19 23:11:40 -0300

    Ajuste 2
    Ajustei algumas coisas estruturais,
    Arrumei erros de lógicas como media geral da turma e adicionei um aluno com sem nota para verificar a funcionalidade do código. E deu erro. Irei reajustar

--- Ex aula 15/ex07/ambiente.js
+++ Ex aula 15/ex07/ambiente.js
@@ -2,7 +2,8 @@ var alunos = [
     { nome: "Ana", notas: [8, 7, 9] },
     { nome: "Carlos", notas: [5, 6, 4] },
     { nome: "Marina", notas: [10, 9, 8] },
-    { nome: "João", notas: [6, 6, 7] }
+    { nome: "João", notas: [6, 6, 7] },
+    { nome: "Pedro", notas: [] }
 ]
 
 //Variáveis
@@ -11,36 +12,37 @@ var maior_nome = ''
 var nota = 0
 var quantidade = 0
 var maior = 0
-
-
+var media = 0
+var todas_notas = 0
+var media_geral = 0
+var quantidade_final = 0 //Vai somar todas às notas dentro do vetor 12(doze) notas.
 for(let i of alunos){
 
     //Variáveis que voltam ao zero
-    var acumula = 0
     var soma = 0
 
     //Variáveis que contem o elemento dentro do vetor
     nota = i.notas
     nomes = i.nome
     quantidade = i.notas.length
-    
+    quantidade_final += i.notas.length
 
     //Calcula as notas dos alunos
     for(let x of nota){
-        soma += x
+        todas_notas += x // Pega a nota de todo mundo e soma
+        media_geral = todas_notas / quantidade_final //Media da turma completa
+
+
+        soma += x  
+        media = soma / quantidade //Media indivudial de cada aluno
+        
+
+        //motrar a maior média de notas e o nome da pessoa com a maior média
+        if(media > maior){
+            maior = media
+            maior_nome = i.nome
+        }
     }
-
-
-    //Variável que calcula à media
-    var media = soma / quantidade
-
-    
-    //motrar a maior média de notas e o nome da pessoa com a maior média
-    if(media > maior){
-        maior = media
-        maior_nome = i.nome
-    }
-    
 
     //Se à media for maior ou igual à 7(sete) aparece a mensagem de aprovado
     if(media >= 7){
@@ -52,10 +54,10 @@ for(let i of alunos){
         console.log(`Nome: ${nomes} || Notas: ${nota} || Nota total: ${soma} || Média: ${media.toFixed(1)} || REPROVADO`)
     }
 
+    
 }
-
-//Médias da turma
-console.log(`Média geral: ${media.toFixed(1)}`)
+//Média geral da turma
+console.log(`Media Da Turma:`, media_geral.toFixed(1))
 
 //Maior média
 console.log(`Maior Média: ${maior_nome} ${maior} 🏆` )//Coloquei o emoji de propósito só para decorar. NÃO FOI CHATGPT!
